2a7310ba2b0fa0d577a16ecc2be1b16c6ca46287
hazelnut6 <hazelpachoco6@gmail.com>
2025-02-09 11:07:21 +0800

    Modify file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-
+import '../css/header.css'
 
 export default function Header({score, bestScore}) {
   return (
-    <div>
-        <h1>Memory card game</h1>
-        <p className="instructions">
-            Instruction: Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laudantium totam saepe incidunt assumenda ex quasi in placeat nam. Eum omnis ipsum ut dolorem, error amet reprehenderit aspernatur dolores laborum.
+    <header>
+        <h1>Memory: Card game</h1>
+        <p className="rule">
+          <span>Instruction:</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laudantium totam saepe incidunt assumenda ex quasi in placeat nam. Eum omnis ipsum ut dolorem, error amet reprehenderit aspernatur dolores laborum.
         </p>
         <p className="scores">
-            <span>Score: {score}</span> <br />
-            <span>Best score: {bestScore}</span>
+          <span>Score: {score}</span> <br />
+          <span>Best score: {bestScore}</span>
         </p>
-    </div>
+    </header>
   )
 }
