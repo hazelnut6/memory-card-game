97de8b657d5622fd4cbc0c9bf3ffc7c6aedb5b91
hazelnut6 <hazelpachoco6@gmail.com>
2025-02-10 13:09:35 +0800

    Modify file

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,16 +3,16 @@ import '../css/cards.css'
 export default function Cards({data, handleClick}) {
   return (
     <div className="board">
-       {data.map((d, i) => {
-            return(
-              <li key={i}>
-                  <div className="card" onClick={() => {handleClick(d)}}>
-                    <img src={d.img} alt={`Image of ${d.name}`} className='pokeImg'/>
-                    <p className='pokeName'>{d.name}</p>
-                  </div>
-              </li>
-            )
-        })} 
+      {data.map((d, i) => {
+        return(
+          <li key={i}>
+            <div className="card" onClick={() => {handleClick(d)}}>
+              <img src={d.img} alt={`Image of ${d.name}`} className='pokeImg'/>
+              <p className='pokeName'>{d.name}</p>
+            </div>
+          </li>
+        )
+      })} 
     </div>
   )
 }
